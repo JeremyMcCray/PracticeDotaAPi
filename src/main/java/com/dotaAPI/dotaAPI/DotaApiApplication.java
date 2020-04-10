68735f2541524c492f2132f8419d44e37a3021bf
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-04-09 23:16:47 -0400

    cool

---
+++ src/main/java/com/dotaAPI/dotaAPI/DotaApiApplication.java
@@ -0,0 +1,13 @@
+package com.dotaAPI.dotaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dotaAPI/dotaAPI/DotaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dotaAPI.dotaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
